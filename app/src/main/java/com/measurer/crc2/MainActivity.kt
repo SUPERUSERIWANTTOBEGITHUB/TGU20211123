948bff17630ff44302e3dd97da56f293059cae73
AS <yvg68@yandex.ru>
2021-11-27 19:11:24 +0300

    delete textView and add random

--- app/src/main/java/com/measurer/crc2/MainActivity.kt
+++ app/src/main/java/com/measurer/crc2/MainActivity.kt
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlin.random.Random
 //import androidx.appcompat.app.AppCompatActivity
 
-// my first comment
-
 class MainActivity<textView> : AppCompatActivity() {
+    lateinit var helloTextView:TextView
+    lateinit var newTextButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val helloTextView:TextView = findViewById(R.id.hello_world_text_view)
-        helloTextView.text = "HI! welcome"
-        val newTextButton:Button = findViewById(R.id.button)
-        newTextButton.setOnClickListener {
-            helloTextView.text = "I am welcome you"
-        }
+        helloTextView = findViewById(R.id.hello_world_text_view)
+        newTextButton = findViewById(R.id.button)
+        newTextButtonClick()
+    }
 
+    fun newTextButtonClick() {
+        newTextButton.setOnClickListener{
+            helloTextView.text = Random.nextInt().toString()
+        }
     }
 }
+
+
 // my second comment
