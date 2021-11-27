569c562dc8fc693213fc471465f202a852a70e63
AS <yvg68@yandex.ru>
2021-11-27 18:53:42 +0300

    add textView and button

--- app/src/main/java/com/measurer/crc2/MainActivity.kt
+++ app/src/main/java/com/measurer/crc2/MainActivity.kt
@@ -2,13 +2,23 @@ package com.measurer.crc2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+//import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+// my first comment
+
+class MainActivity<textView> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloTextView:TextView = findViewById(R.id.hello_world_text_view)
+        helloTextView.text = "HI! welcome"
+        val newTextButton:Button = findViewById(R.id.button)
+        newTextButton.setOnClickListener {
+            helloTextView.text = "I am welcome you"
+        }
+
     }
 }
-// my first comment
-
 // my second comment
